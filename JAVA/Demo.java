a5609eb6fe508bf8f945390562baab34952d5390
Aditya Maitreya <149099766+adityamaitreya@users.noreply.github.com>
2025-05-18 19:35:58 +0530

    hello

---
+++ JAVA/Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
+
+// PS E:\Code_Space\JAVA> javac .\Demo.java
+// PS E:\Code_Space\JAVA> java Demo
